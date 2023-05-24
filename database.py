1f3cefc0c1d8caba3ba7c9438956fe4f8b0a62ae
bjornarron <55078095+bjornarron@users.noreply.github.com>
2023-05-24 15:53:19 +0200

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import datetime
 
 # PostgreSQL connection details
 conn_params = {
-    "host": "localhost",
+    "host": "172.24.0.3",
     "port": "5432",
     "database": "postgres",
     "user": "postgres",
